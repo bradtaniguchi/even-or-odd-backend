82b5d3ef629f42f2a42b30a031786c947d44cdcc
brad <bradtaniguchi@gmail.com>
2017-09-11 22:00:38 -0700

    Added bet capture, to interact with the front-end

--- sockets/index.js
+++ sockets/index.js
@@ -4,7 +4,15 @@ module.exports.listen = (app) => {
   io = socketio.listen(app);
   io.sockets.on('connection', (socket) => {
     console.log('sockets:connection');
+
+    socket.on('bet', (from, msg) => {
+      console.log('test in bet');
+      console.log(from);
+      console.log(msg);
+    });
   });
+
+  // new room test
   const messages = io.of('/messages');
   messages.on('connection', (socket) => {
     console.log('messages:connection');
