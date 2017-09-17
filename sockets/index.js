bbc978d2ea20b4c880193e1381eb312b6b3572de
brad <bradtaniguchi@gmail.com>
2017-09-17 11:41:12 -0700

    Added 'un' hooks to current actions, and disconnect

--- sockets/index.js
+++ sockets/index.js
@@ -2,15 +2,30 @@ const socketio = require('socket.io');
 
 module.exports.listen = (app) => {
   io = socketio.listen(app);
+  
   io.sockets.on('connection', (socket) => {
     console.log('sockets:connection');
-
+  // normal bet
     socket.on('bet', (from, msg) => {
       console.log('test in bet');
       console.log(from);
       console.log(msg);
+      socket.broadcast.emit('bet', from, msg); // send information to other players
     });
-  });
+    //
+    socket.io('unbet', (from, msg) => {
+      console.log('test in unbet');
+      console.log(from);
+      console.log(msg);
+      socket.broadcast.emit('unbet', from, msg); // send information to other players
+    });
+
+    socket.on('disconnect', (first, secon) => {
+      console.log('test in disconnect');
+      console.log(first);
+      console.log(second);
+    });
+  }); // end connection
 
   // new room test
   const messages = io.of('/messages');
